7d851ca15949d19107929d7db7b08f243bb1b2f3
Christia T <christiatidwell@bellsouth.net>
2018-08-08 18:24:54 +0000

    automatically backed up by learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,4 +1,4 @@
-## Code your solution below. Note that your SQL queries should be in quotation marks. 
+## Code your solution below. Note that your SQL queries should be in quotation marks.
 
 def highest_student_gpa
   "SELECT MAX(column_name) FROM table_name;"
@@ -9,6 +9,7 @@ def lowest_student_gpa
 end
 
 def average_student_gpa
+  "SELECT AVG(column_name) FROM table_name;"
 end
 
 def total_tardies_for_all_students
