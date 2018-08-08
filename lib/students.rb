c771576f5d0aea110afa6f132ae148e3e74a844b
Christia T <christiatidwell@bellsouth.net>
2018-08-08 18:28:18 +0000

    automatically backed up by learn

--- lib/students.rb
+++ lib/students.rb
@@ -5,11 +5,11 @@ def highest_student_gpa
 end
 
 def lowest_student_gpa
-  "SELECT MIN(column_name) FROM table_name;"
+  "SELECT MIN(gpa) FROM students;"
 end
 
 def average_student_gpa
-  "SELECT AVG(column_name) FROM table_name;"
+  "SELECT AVG(gpa) FROM students;"
 end
 
 def total_tardies_for_all_students
